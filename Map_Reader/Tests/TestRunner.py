1731789654439f5be64e5d2fbc0b055efcd9e6d6
ebrittain <32505736+ebrittain@users.noreply.github.com>
2019-09-19 11:52:21 -0700

    Update TestRunner.py

--- Map_Reader/Tests/TestRunner.py
+++ Map_Reader/Tests/TestRunner.py
@@ -1,8 +1,27 @@
 import subprocess
 import os
 import re
+import argparse
 
-test_files = [x for x in os.listdir() if re.search(r'.*_test.py', x)]
+parser = argparse.ArgumentParser()
+parser.add_argument('--files', type=str, help='Comma separated test filenames')
+args = parser.parse_args()
 
-for test in test_files:
-    subprocess.Popen(f'pytest {test}', subprocess.STARTF_USESTDHANDLES | subprocess.STARTF_USESHOWWINDOW).communicate()
+def run_tests(tests):
+	for test in tests:
+		subprocess.Popen(f'pytest {test}', subprocess.STARTF_USESTDHANDLES | subprocess.STARTF_USESHOWWINDOW).communicate()
+
+test_files = {x for x in os.listdir() if re.search(r'.*_test.py', x)}
+
+if args.files:
+	arg_files = set(args.files.split(','))
+	test_files = test_files.intersection(arg_files)
+
+	if test_files != arg_files:
+		print('Invalid Files:', ', '.join(arg_files-test_files))
+		parser.print_help()
+	else:
+		run_tests(test_files)
+
+else:
+	run_tests(test_files)
